5c914ccb783fc50ca47bb70842088e313606065c
Ved <77605086+Ved37@users.noreply.github.com>
2022-05-07 00:33:12 +0530

    Added icon and changed card nine content

--- src/CardFive.js
+++ src/CardFive.js
@@ -7,8 +7,8 @@ export default function CardFive() {
       <p className="title">BEST CREATIVE HACK</p>
         <hr className="line"></hr>
         <ul className="comp-content">
-          <li>KH, 5, Rd No. 5, Sector 15, Gandhinagar, Gujarat 382016</li>
-          <li>28th March 2022, Monday</li>
+          <li className='loc'>KH, 5, Rd No. 5, Sector 15, Gandhinagar, Gujarat 382016</li>
+          <li className='dat'>28th March 2022, Monday</li>
         </ul>
       </div>
     </div>
--- src/CardNine.js
+++ src/CardNine.js
@@ -4,11 +4,11 @@ export default function CardNine() {
   return (
     <div className="flex-container">
       <div className="comp-card">
-        <p className="title">BEST CLOUD BASED HACK</p>
+      <p className="title">BEST CREATIVE HACK</p>
         <hr className="line"></hr>
         <ul className="comp-content">
-          <li>Powerpoint Presentation(As Format given by SIH 2022)</li>
-          <li>Prototype for the selected problem statement</li>
+        <li className='loc'>KH, 5, Rd No. 5, Sector 15, Gandhinagar, Gujarat 382016</li>
+          <li className='dat'>28th March 2022, Monday</li>
         </ul>
       </div>
     </div>
